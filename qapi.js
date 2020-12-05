9983bd5be548a0d2eb377e89c8f4c92531130ec4
yugg9826 <71370210+yugg9826@users.noreply.github.com>
2020-12-05 12:43:49 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcb2f27f8ae59001fb0c6d8",
+				"5fcb2f27f8ae59001fb0c6da"
+			]
+		},
+		"5fcb2f27f8ae59001fb0c6d8": {
+			"id": "5fcb2f27f8ae59001fb0c6d8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcb2f27f8ae59001fb0c6da": {
+			"id": "5fcb2f27f8ae59001fb0c6da",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
